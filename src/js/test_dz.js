81ac3bae592e910d3056f9558f243a6f9bfa6fa5
nltesown <nlte@magic.fr>
2014-04-11 15:49:07 +0200

    New DeepZoom instance on each view

--- src/js/test_dz.js
+++ src/js/test_dz.js
@@ -2,57 +2,72 @@
 
 $(function () {
 	"use strict";
-	var $overlay = $("<div class='overlay'></div>").appendTo($("body")),
-		deepZoom;
-
-	deepZoom = new DeepZoom({
-		$el: $(".overlay"), // Container element
-		zoomControls: true,	// Zoom controls
-		closeButton: true,  // Close button
-		baseUrl: "http://cf.pasoliniroma.com/static/langlois/dz/"
-	});
+	var $overlay = $("<div class='overlay'></div>").appendTo($("body"));
 
 	$("a.openbutton").on("click", function () {
 		var data = $(this).data("dz");
-		deepZoom.open(data.folder, data.width, data.height);
+		var deepZoom = new DeepZoom({
+			$el: $(".overlay"),
+			url: "http://cf.pasoliniroma.com/static/langlois/dz/" + data.folder,
+			width: data.width,
+			height: data.height
+		});
 	});
-
 });
 
 
 
-
-
 // DeepZoom
-var DeepZoom = function ( options ) {
+var DeepZoom = function (options) {
+	options = $.extend({ // Defaults
+		zoomControls: true,
+		closeButton: true
+	}, options);
+
+
 	var $el = options.$el.eq(0),
-		baseUrl = options.baseUrl,
+		url = options.url.replace(/\/?$/, "/"), // With trailing "/"
 		zoomControls = !!options.zoomControls,
 		closeButton = !!options.closeButton,
+		width = options.width,
+		height = options.height,
 		id = $el.attr("id");
 
 	if (_.isUndefined($el)) {
 		throw "DeepZoom: container element not specified";
 	}
 
-	// If $el doesn't have a valid id, we give it our own (dzcontainer)
+	// If $el doesn't have a (valid) id, we give it our own ("dzcontainer")
 	if (id === void 0 || id.match(/^[a-zA-Z][\w:.-]*$/g) === null) {
 		id = $el.attr("id", "dzcontainer").attr("id");
 	}
 
-	$el.css({ cursor: "move" });
-
-	this.baseUrl = baseUrl;
+	this.url = url;
 	this.$el = $el;
 	this.id = id;
-	this.viewer = OpenSeadragon({
+	this.width = width;
+	this.height = height;
+	this.closeButton = closeButton;
+	this.zoomControls = zoomControls;
+
+	this.open();
+};
+
+
+DeepZoom.prototype.open = function () {
+
+	if (this.$el.is(":visible") === false) { // ?
+		this.$el.fadeIn(500);
+	}
+
+	this.viewer = OpenSeadragon({ // TODO: parameterize options
         id: this.id,
         autoHideControls: true,
         wrapHorizontal: false, // Horizontal repeat
         wrapVertical: false, // Vertical repeat
         animationTime: 1.5,
         minZoomImageRatio: .5, // (Min size relative to viewport size)
-        maxZoomPixelRatio: 4, // (Max size relative to real-size pixel image)
+        maxZoomPixelRatio: 2, // (Max size relative to real-size pixel image)
         showNavigationControl: false, // Important
         showNavigator: false,
         mouseNavEnabled: true,
@@ -62,28 +77,53 @@ var DeepZoom = function ( options ) {
         zoomPerScroll: 1.1 // Default: 1.2
 	});
 
-	this.closeButton = closeButton;
-	this.zoomControls = zoomControls;
+	this.viewer.open({
+		Image: {
+			xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+			Url: this.url,
+			Format: "jpg",
+			Overlap: "1",
+			TileSize: "256",
+			Size: {
+				Height: this.height.toString(),
+				Width: this.width.toString()
+			}
+		}
+	});
 
-	if (zoomControls || closeButton) {
+	if (this.zoomControls || this.closeButton) {
 		this.initControls();
 	}
+
+	this.$el.css({ cursor: "move" });
+
+	this.isOpen = true;
 };
 
 
-DeepZoom.prototype = (function () {
-	var close, closeWindow, initControls, open, setVisible;
+// DeepZoom.prototype.close = function (e) {
+// 	var self = e.data;
+// 	self.$el.fadeOut(500, function () {
+// 		self.viewer.destroy();
+// 	});
+// }
+DeepZoom.prototype.close = function () {
+	var self = this;
+	this.$el.fadeOut(500, function () {
+		self.viewer.destroy();
+	});
+}
 
-	initControls = function () {
-		var $slider, $closeButton,
-			self = this;
 
+
+DeepZoom.prototype.initControls = function () {
+	var $slider,
+		$closeButton,
+		self = this;
+
+	if (this.zoomControls) {
 		$("<div class='dzzoomcontrols'><div class='dzscalebutton minus'></div><div class='dzslider'></div><div class='dzscalebutton plus'></div></div>").appendTo(this.$el);
 		$slider = this.$el.find(".dzslider");
-		$closeButton = $("<div class='dzclosebutton'></div>").appendTo(this.$el);
-
-	    $("body").spotlight({ fadeables: ".dzzoomcontrols, .dzclosebutton", interval: 800});
-
 		this.viewer.addHandler("open", function () {
 			$slider.slider({
 				min: minZoomLevel(),
@@ -93,99 +133,51 @@ DeepZoom.prototype = (function () {
 					return self.viewer.viewport.zoomTo(reverseZoomLevel(r.value));
 				}
 			});
-
 	        self.viewer.addHandler("zoom", function () {
 	            $slider.slider("value", currentZoomLevel());
 	        });
-
 		});
-
-        $(".dzscalebutton.minus").on("click", zoomOut);
-        $(".dzscalebutton.plus").on("click", zoomIn);
-        $(".dzclosebutton").on("click", function () { self.closeWindow(self); });
-
-	    function convertZoomLevel (level) { // OSD zoom to UI zoom
-	        return 100 * Math.log(level / self.viewer.viewport.getMinZoom()) / Math.log(2);
-	    }
-
-	    function reverseZoomLevel (level) { // UI zoom to OSD zoom
-	        return self.viewer.viewport.getMinZoom() * Math.pow(2, level / 100)
-	    }
-
-	    function minZoomLevel () {
-	        return convertZoomLevel(null != (t = self.viewer.viewport) ? t.getMinZoom() : void 0);
-	    }
-
-	    function maxZoomLevel () {
-	        return convertZoomLevel(null != (t = self.viewer.viewport) ? t.getMaxZoom() : void 0);
-	    }
-
-	    function currentZoomLevel () {
-	        return convertZoomLevel(null != (t = self.viewer.viewport) ? t.getZoom() : void 0);
-	    }
-
-	    function zoomIn () {
-	        return triggerZoomBy(self.viewer.zoomPerClick);
-	    }
-
-	    function zoomOut () {
-	       return triggerZoomBy(1 / self.viewer.zoomPerClick);
-	    }
-
-	    function triggerZoomBy (t) {
-	        self.viewer.viewport.zoomBy(t);
-	        self.viewer.viewport.applyConstraints(); // NB: method not documented (http://openseadragon.github.io/docs/symbols/OpenSeadragon.Viewport.html#applyConstraints)
-	    }
-	};
-
-	close = function () { // Close current image in the viewer
-		this.viewer.close();
-	};
-
-	closeWindow = function (self) {
-		self.close(); // Remove view
-		self.viewer.removeAllHandlers();
-		self.$el.hide();
-	};
-
-	destroy = function () { // Destroy viewer
-		this.viewer.destroy();
 	}
 
+	if (this.closeButton) {
+		$closeButton = $("<div class='dzclosebutton'></div>").appendTo(this.$el);
+		$closeButton.on("click", function () { self.close(); });
+	}
 
-	// TODO: reset zoom level display on the slider! (remains set from previous view)
-	open = function (folderName, width, height) {
-		this.viewer.open({
-			Image: {
-				xmlns: "http://schemas.microsoft.com/deepzoom/2008",
-				Url: this.baseUrl + folderName + "/",
-				Format: "jpg",
-				Overlap: "1",
-				TileSize: "256",
-				Size: {
-					Height: height.toString(),
-					Width: width.toString()
-				}
-			}
-		});
+    $("body").spotlight({ fadeables: ".dzzoomcontrols, .dzclosebutton", interval: 800});
 
-		if (this.$el.is(":visible") === false) {
-			this.$el.show();
-		}
+    function convertZoomLevel (level) { // OSD zoom to UI zoom
+        return 100 * Math.log(level / self.viewer.viewport.getMinZoom()) / Math.log(2);
+    }
 
-	};
+    function reverseZoomLevel (level) { // UI zoom to OSD zoom
+        return self.viewer.viewport.getMinZoom() * Math.pow(2, level / 100)
+    }
 
-	setVisible = function (visible) {
-		this.viewer.setVisible(!!visible);
-	};
+    function minZoomLevel () {
+        return convertZoomLevel(null != (t = self.viewer.viewport) ? t.getMinZoom() : void 0);
+    }
 
-	return {
-		// constructor: DeepZoom, // Necessary? http://stackoverflow.com/questions/55611/javascript-private-methods
-		close: close,
-		closeWindow: closeWindow,
-		destroy: destroy,
-		initControls: initControls,
-		open: open,
-		setVisible: setVisible
-	};
-}());
+    function maxZoomLevel () {
+        return convertZoomLevel(null != (t = self.viewer.viewport) ? t.getMaxZoom() : void 0);
+    }
+
+    function currentZoomLevel () {
+        return convertZoomLevel(null != (t = self.viewer.viewport) ? t.getZoom() : void 0);
+    }
+
+    function zoomIn () {
+        return triggerZoomBy(self.viewer.zoomPerClick);
+    }
+
+    function zoomOut () {
+       return triggerZoomBy(1 / self.viewer.zoomPerClick);
+    }
+
+    function triggerZoomBy (t) {
+        self.viewer.viewport.zoomBy(t);
+        self.viewer.viewport.applyConstraints(); // NB: method not documented (http://openseadragon.github.io/docs/symbols/OpenSeadragon.Viewport.html#applyConstraints)
+    }
+};
+
+
