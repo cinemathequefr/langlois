71ee6dd0e30b91cc1cffee0ac6ced8ac495ce6bb
nltesown <nlte@magic.fr>
2014-05-06 12:49:10 +0200

    Concatenate js/css

--- gulpfile.js
+++ gulpfile.js
@@ -8,36 +8,35 @@ var minifycss = require("gulp-minify-css");
 gulp.task("default", function () {
 
 	gulp.src([ // Note: List the scripts in the order in which they must be concatenated
-        "src/js/vendor/jquery.easing.1.3.min.js",
-        "src/js/vendor/jquery.animate-enhanced.min.js",
-        "src/js/vendor/jquery.scrollTo.min.js",
-        "src/js/vendor/jquery.debouncedresize.js",
-        "src/js/vendor/path.js",
-        "src/js/vendor/mustache.js",
-        "src/js/vendor/jquery.imagesloaded.min.js",
-        "src/js/vendor/openseadragon.min.js",
-        "src/js/vendor/spotlight.js",
-        "src/js/vendor/jquery-ui-1.10.4.custom.min.js",
-        "src/js/vendor/perfect-scrollbar-0.4.10.with-mousewheel.min.js",
-        "src/js/fitInBox.js",
-        "src/js/vendor/jquery.mCustomScrollbar.min.js",
-        "src/js/timeline.js",
-        "src/js/quadrant.js",
-        "src/js/deepzoom.js",
-        "src/js/application.js"
+                "src/js/vendor/jquery.easing.1.3.min.js",
+                "src/js/vendor/jquery.animate-enhanced.min.js",
+                "src/js/vendor/jquery.scrollTo.min.js",
+                "src/js/vendor/jquery.debouncedresize.js",
+                "src/js/vendor/path.js",
+                "src/js/vendor/mustache.js",
+                "src/js/vendor/jquery.imagesloaded.min.js",
+                "src/js/vendor/openseadragon.min.js",
+                "src/js/vendor/spotlight.js",
+                "src/js/vendor/jquery-ui-1.10.4.custom.min.js",
+                "src/js/vendor/perfect-scrollbar-0.4.10.with-mousewheel.min.js",
+                "src/js/fitInBox.js",
+                "src/js/vendor/jquery.mCustomScrollbar.min.js",
+                "src/js/timeline.js",
+                "src/js/quadrant.js",
+                "src/js/deepzoom.js",
+                "src/js/application.js"
 	])
 	.pipe(concat("main.js"))
 	.pipe(uglify())
 	.pipe(gulp.dest("js"));
 
 	gulp.src([
-        "src/css/normalize.css",
-        "src/css/jquery.mCustomScrollbar.css",
-        "src/css/timeline.css",
-        "src/css/main.css"
+                "src/css/jquery.mCustomScrollbar.css",
+                "src/css/timeline.css",
+                "src/css/main.css"
 	])
 	.pipe(concat("main.css"))
-	.pipe(minifycss())
+	//.pipe(minifycss())
 	.pipe(gulp.dest("css"));
 
 });
